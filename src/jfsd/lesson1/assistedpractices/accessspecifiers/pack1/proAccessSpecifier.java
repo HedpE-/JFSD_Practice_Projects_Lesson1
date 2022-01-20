8157170c05e930d0a1e0b422a259482de9534f80
Rui Gonçalves <rui.goncalves3@vodafone.com>
2022-01-20 00:22:00 +0000

    Removed Assisted practices

--- src/jfsd/lesson1/assistedpractices/accessspecifiers/pack1/proAccessSpecifier.java
+++
@@ -1,9 +0,0 @@
-package jfsd.lesson1.assistedpractices.accessspecifiers.pack1;
-
-public class proAccessSpecifier {
-
-	protected void display() 
-	{ 
-		System.out.println("This is protected access specifier"); 
-	} 
-}
--- src/jfsd/lesson1/assistedpractices/accessspecifiers/pack1/pubAccessSpecifiers.java
+++
@@ -1,9 +0,0 @@
-package jfsd.lesson1.assistedpractices.accessspecifiers.pack1;
-
-public class pubAccessSpecifiers {
-	
-	public void display() 
-    { 
-        System.out.println("This is Public Access Specifiers"); 
-    }
-}
